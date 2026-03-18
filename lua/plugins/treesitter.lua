e34f32fd6121c8c1ff8cc35e54b0d50c3a12ca45
Leny Peris <lenyperis@hotmail.fr>
2026-03-18 15:52:52 +0100

    FIX [TREESITTER] : une journée en enfer pour de jolies couleurs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,29 @@
-require'nvim-treesitter.configs'.setup
+--[[require'nvim-treesitter'.setup
 {
 	ensure_installed = {"c", "lua"},
 	auto_install = true,
 	highlight = {enable = true},
-}
+}--]]
 
+--vim.opt.runtimepath:append("/home/lperis/.local/share/nvim/site")
+
+vim.opt.runtimepath:append("/home/lperis/.local/share/nvim/site/")
+
+-- v1.x API: highlighting is enabled via vim.treesitter directly
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
+
+-- Still call setup for ensure_installed / auto_install
+require('nvim-treesitter').setup({
+  ensure_installed = { "c", "lua" },
+  auto_install = true,
+})
+
+require('nvim-treesitter').setup({
+  ensure_installed = { "c", "lua" },
+  auto_install = true,
+  highlight = { enable = true },
+})
